2f11d142d6f4d59e4034040d134f0fb8af0df04e
Ainedembe Denis <dembedenisjb@gmail.com>
2019-08-24 05:47:58 +0300

    Assign variables to every post data and send it to awamo_mathjs_api_post() function

--- application/modules/api/controllers/Api.php
+++ application/modules/api/controllers/Api.php
@@ -10,6 +10,8 @@ class Api extends MX_Controller {
 		$this->load->model('api_mdl');
         $this->module="api";
 
+        //Api URL resource
+        $this->API_URL = "http://api.mathjs.org/v4/";
 		
 	}
 
@@ -18,7 +20,27 @@ class Api extends MX_Controller {
 		$post_data = $this->input->post();
 
 		//print_r($post_data);
-		return $post_data;
+		//return $post_data;
+
+		//assign variables to each of the post data
+		$num1 = $post_data['num1'];
+		$num2 = $post_data['num2'];
+		$opr = $post_data['operand'];
+
+		//send data to api_post function
+		$this->awamo_mathjs_api_post($num1, $num2, $opr);
 	}
+
+
+	//Api post data function
+	public function awamo_mathjs_api_post($num1=false, $num2=false, $opr=false){
+
+
+	}
+
+
+
+
+
 
 }
